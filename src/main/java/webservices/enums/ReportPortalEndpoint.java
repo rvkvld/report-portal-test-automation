46a804ea3ffb3d7206ae7816a994bf59959cac59
rvkvld <rvk.vlad@gmaiil.com>
2023-10-16 22:09:03 -0500

    Remove annotation

--- src/main/java/webservices/enums/ReportPortalEndpoint.java
+++ src/main/java/webservices/enums/ReportPortalEndpoint.java
@@ -1,15 +1,12 @@
 package webservices.enums;
 
-import lombok.Getter;
 
-@Getter
 public enum ReportPortalEndpoint {
-
     GET_HEALTH("/health");
 
     private String endpoint;
 
     ReportPortalEndpoint(String endpoint) {this.endpoint = endpoint;}
 
-//    public String getEndpoint() { return endpoint;}
+    public String getEndpoint() { return endpoint;}
 }
