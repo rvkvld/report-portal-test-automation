b81ea86a71f7fdfd6252951f965cbcf736782d16
rvkvld <rvk.vlad@gmaiil.com>
2024-01-17 23:16:26 -0600

    Add pipeline steps

--- jenkinsfiles/pipelineGit.groovy
+++ jenkinsfiles/pipelineGit.groovy
@@ -1,7 +1,26 @@
 pipeline {
-    agent { docker { image 'mcr.microsoft.com/playwright/java:v1.40.0-jammy' } }
-    stages {
-        stage('e2e-tests') {
+    agent {label 'any'}
+    stages{
+        stage('Pre-Build Cleanup') {
+            steps {
+                deleteDir() //This will remove the workspace before each build
+            }
+        }
+        stage ('Checkout Repo'){
+            steps {
+                script {
+                    deleteDir()
+                    checkout([$class: 'GitSCM', branches: [[name: '$branch']]])
+                }
+            }
+        }
+        stage ('e2e-test') {
+            agent {
+                docker {
+                    image 'mcr.microsoft.com/playwright/java:v1.40.0-jammy'
+                    reuseNode true
+                }
+            }
             steps {
                 sh 'mvn -B install -D skipTests --no-transfer-progress'
                 sh 'mvn test'
